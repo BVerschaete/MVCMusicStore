fd603f253da6249c37d722a0453b938b68b5b16d
BVerschaete <bastien.verschaete@gmail.com>
2016-10-17 21:40:35 +0200

    Initial commit

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStore.ViewModels;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+			var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+
+			var viewModel = new StoreIndexViewModel
+			{
+				NumberOfGenres = genres.Count(),
+				Genres = genres
+			};
+            return View(viewModel);
+        }
+
+		public ActionResult Movie()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Movie(MovieViewModel movie)
+		{
+			if(movie != null)
+			{
+				movie.stars++;
+			}
+
+			ModelState.Clear(); //!!!oorspronkelijke waarden clearen en nieuwe er terug in zetten
+
+			return View(movie);
+		}
+    }
+}
---
+++ MVCMusicStore/ViewModels/MovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.ViewModels
+{
+	public class MovieViewModel
+	{
+		public string name{ get; set; }
+
+		public int stars { get; set; }
+
+		public string director{ get; set; }
+	}
+}
